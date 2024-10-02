8e9d238739d4dde00fe6f7ea6e84e248aeb563c0
TheSlopster <166720394+TheSlopster@users.noreply.github.com>
2024-10-02 14:26:58 +0100

    V0.3
    UI/Menus Done

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,9 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame()
+    public void TestLevel()
     {
         SceneManager.LoadSceneAsync(1);
+    }
+
+    public void Junglelevel()
+    {
+        SceneManager.LoadSceneAsync(3);
+    }
+
+    public void Retrolevel()
+    {
+        SceneManager.LoadSceneAsync(5);
+    }
+
+    public void Spacelevel()
+    {
+        SceneManager.LoadSceneAsync(4);
     }
 
     public void QuitGame()
